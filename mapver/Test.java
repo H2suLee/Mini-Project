a746e699c01e31f492cf4a791eae47bf783f4611
H2suLee <103907953+H2suLee@users.noreply.github.com>
2022-05-14 19:58:39 +0900

    Delete mapver directory

--- mapver/Test.java
+++
@@ -1,9 +0,0 @@
-package co.micol.miniprj.mapver;
-
-public class Test {
-	public static void main(String[] args) {
-		Menu menu = new Menu();
-		menu.run();
-//		System.out.println(Puppy.pup.toString());
-	}
-}
