ded8cec935a788b9d9d87899fc05cc9df5340fa7
H2suLee <103907953+H2suLee@users.noreply.github.com>
2022-05-16 17:01:12 +0900

    Add files via upload
    finale_cmd print version_

---
+++ listver/listversion/src/main/java/co/self/miniprj/finale/listversion/App.java
@@ -0,0 +1,16 @@
+package co.self.miniprj.finale.listversion;
+
+import co.self.miniprj.listver.Menu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Menu menu = new Menu();
+		menu.run();
+    }
+}
